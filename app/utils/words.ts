9cb0a52b5ee1218fc6b1f00fb4dd326ebe217712
smallStall <91565962+smallStall@users.noreply.github.com>
2023-10-06 22:36:00 +0900

    Update words.ts

--- app/utils/words.ts
+++ app/utils/words.ts
@@ -1,14 +1,14 @@
 export const words = [
   "make each program do one thing well",
   "Worse is better",
-  "export const hoge = 957",
-  "class Hoge{",
-  "<div className='hoge'",
+  "export const hoge",
+  "class Hoge",
+  "div className",
   "def fib_print(n)",
-  "const func = () => {",
+  "const func =",
   "return (",
   "font-size: 3em",
   "line-height: 1.7rem",
-  "background-color: black",
-  "const [isPushed, setIsPushed] ="
+  "background-color",
+  "const isPushed, setIsPushed"
 ]
